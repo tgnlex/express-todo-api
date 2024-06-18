53f1f47ab48cbc6bce80576e201febfac0c8c664
Tgn <114723993+tgnlex@users.noreply.github.com>
2024-06-17 20:20:04 -0400

    Update index.tsx

--- frontend/src/pages/Chat/index.tsx
+++ frontend/src/pages/Chat/index.tsx
@@ -1,10 +1,9 @@
-import './style.styl';
-
+import {Chatroom} from '../../components/chat/Chatroom';
 function Chat() {
 	return (
 		<main id="chat"class="container">
 			<h1 id="heading">Chat Room</h1>
-   
+   			<Chatroom />
 		</main>
 	);
 }
